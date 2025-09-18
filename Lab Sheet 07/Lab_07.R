61f2f5e8c545433200c229f2e2b38d320167a657
IT24101605 <it24101605@my.sliit.lk>
2025-09-18 20:45:08 +0530

    Lab_07_answers

---
+++ Lab Sheet 07/Lab_07.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\03cri\\Desktop\\IT24101605")
+
+# Question 01
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+
+# Question 02
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+
+# Question 03
+# i
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+
+# ii
+qnorm(0.95, mean=100, sd=15, lower.tail=TRUE)
+
