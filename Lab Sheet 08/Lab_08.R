72aa52a0e0e65fc8ef8024221ed7c4de61683a19
IT24101605 <it24101605@my.sliit.lk>
2025-09-26 11:02:41 +0530

    Lab_08_answers

---
+++ Lab Sheet 08/Lab_08.R
@@ -0,0 +1,41 @@
+setwd("C:\\Users\\it24101605\\Desktop\\IT24101605_Lab_08")
+
+data <- read.table("Exercise - LaptopsWeights.txt", header=TRUE)
+fix(data)
+attach(data)
+
+## Question 01
+popmn <- mean(Weight.kg.)
+popvar <- var(Weight.kg.) * (length(Weight.kg.)-1)/length(Weight.kg.)
+popSD <- sqrt(popvar)
+
+popmn
+popvar
+popSD
+
+
+## Question 02
+samples <- c()
+n <- c()
+for (i in 1:25){
+  s <- sample(Weight.kg., 6, replace=TRUE)
+  samples <- cbind(samples, s)
+  n <- c(n,paste('S',i))
+}
+colnames(samples) <- n
+
+s.means <- apply(samples, 2, mean)
+s.var   <- apply(samples, 2, var)
+s.SD    <- sqrt(s.var)
+
+s.means
+s.SD
+
+
+## Question 03
+mean_smeans <- mean(s.means)
+sd_smeans   <- sd(s.means)
+
+mean_smeans
+sd_smeans
+
